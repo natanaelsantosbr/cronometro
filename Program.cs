987c415e8523690f7ffcde04215977c7bce1ce52
Natanael <--replace-all>
2021-09-02 21:12:08 -0300

    substring

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Stopwatch
 {
@@ -6,7 +7,38 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+
+        }
+
+        static void Menu()
+        {
+            System.Console.WriteLine("S - Segundo > 10s = 10 segundos");
+            System.Console.WriteLine("M - Minuto = 1= 1 minuto");
+            System.Console.WriteLine("0 - Sair");
+            System.Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            System.Console.WriteLine(type);
+        }
+
+        static void Start(int time)
+        {
+
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                System.Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            System.Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
         }
     }
 }
