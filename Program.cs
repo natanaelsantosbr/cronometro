04cd25504673d2555e16a3ad65c3cc9787e11642
Natanael <--replace-all>
2021-09-02 21:17:47 -0300

    Calcular tempo

--- Program.cs
+++ Program.cs
@@ -21,8 +21,16 @@ namespace Stopwatch
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length - 1, 1));
             int time = int.Parse(data.Substring(0, data.Length - 1));
-            System.Console.WriteLine(type);
-            System.Console.WriteLine(time);
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            Start(time * multiplier);
+
         }
 
         static void Start(int time)
