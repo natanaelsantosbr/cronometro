4cda259fb431e13bdaed182a23f846f71e587701
Natanael <--replace-all>
2021-09-02 21:20:17 -0300

    Finalizando a aplicação

--- Program.cs
+++ Program.cs
@@ -29,7 +29,20 @@ namespace Stopwatch
             if (time == 0)
                 System.Environment.Exit(0);
 
-            Start(time * multiplier);
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            System.Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            System.Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            System.Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
 
         }
 
